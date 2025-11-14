2fd1d18cbebb13ca0a56624d52178b49072ca11d
Windroid234 <ddidjdu62@gmail.com>
2025-11-14 19:07:27 +0300

    Door fix

--- door.gd
+++ door.gd
@@ -1,7 +1,13 @@
 extends Area2D
 
 
-
-
 func _on_body_entered(body: Node2D) -> void:
-	print ("game")
+	print("[door] _on_body_entered fired. body=", body, " name=", body.name, " class=", body.get_class())
+	if not (body is Player):
+		print("[door] Ignoring - entering body is not Player")
+		return
+	print("[door] Player detected. Changing scene to room_1.tscn...")
+	var err = get_tree().change_scene_to_file("res://room_1.tscn")
+	print("[door] change_scene_to returned:", err)
+	if err != OK:
+		push_error("[door] Failed to change scene to res://room_1.tscn (error: %d)" % err)
